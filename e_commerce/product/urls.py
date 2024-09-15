af2115760d4ee934cb256d9c1bb64af74d78fd8f
Kiarash-gh <kiarash.gh@gmail.com>
2024-09-15 14:30:50 +0330

    feat: list product and show product details

--- e_commerce/product/urls.py
+++ e_commerce/product/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('all-products/', views.all_products, name='all-products'),
+    path('product-detail/<str:pk>', views.product_detail, name='product-detail'),
 ]
--- e_commerce/product/views.py
+++ e_commerce/product/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, ProductSpecification
 
 # Create your views here.
 def home(request):
     context = {}
     return render(request, 'product/home.html', context)
+
+
+def all_products(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'product/all_products.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    product_specifications =ProductSpecification.objects.select_related('specification').filter(product=product)
+    context = {'product': product, 'product_specifications': product_specifications}
+    return render(request, 'product/product_detail.html', context)
